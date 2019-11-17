63f4d1539ae816d7def89a5a8517d8756c8aa67c
psato <forhighlow@gmail.com>
2019-11-17 11:41:03 +0900

    bootstrap導入

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -1,6 +1,7 @@
 """このファイルでは、http://127.0.0.1:5000にリクエストがあったときの処理を書いている"""
+from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 
 @app.route('/')
 def show_entries():             # # http://127.0.0.1:5000にリクエストがあった時、以下のメソッドが呼び出される
-    return "Hello World!"
+    return render_template('entires/index.html') # flaskではtemplatesはhtmlがあると自動で認識するため
