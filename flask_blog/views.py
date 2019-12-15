414033ee85167773618f126dd90f732c4cd52ba7
psato <forhighlow@gmail.com>
2019-12-16 02:28:07 +0900

    entriesがタイポで参照されなかったバグを解消

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -1,7 +1,24 @@
-"""このファイルでは、http://127.0.0.1:5000にリクエストがあったときの処理を書いている"""
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 
+
 @app.route('/')
-def show_entries():             # # http://127.0.0.1:5000にリクエストがあった時、以下のメソッドが呼び出される
-    return render_template('entires/index.html') # flaskではtemplatesはhtmlがあると自動で認識するため
+def show_entries():
+    return render_template('entries/index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            print('ユーザ名が異なります')
+        elif request.form['password'] != app.config['PASSWORD']:
+            print('パスワードが異なります')
+        else:
+            return redirect('/')
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    return redirect('/')
