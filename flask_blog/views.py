269861e2c8a483b3a79f9aa6fcd70b8ddeb35e1e
psato <forhighlow@gmail.com>
2019-12-16 11:24:23 +0900

    sessionをブラウザにキャッシュとして保存し,sessionが正しいならログインしていると認識する

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -4,7 +4,10 @@ from flask_blog import app
 
 @app.route('/')
 def show_entries():
+    if not session.get('logged_in'):
+        return redirect('/login')  
     return render_template('entries/index.html')
+
 
 
 @app.route('/login', methods=['GET', 'POST'])
